3d0ee26035e6acf5866647d22a77c7eb0d1de30e
Malin Lundberg <malin.lundberg91@gmail.com>
2017-03-31 16:35:34 +0200

    basic building blocks

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+require('../main.css');
+
+var Main = React.createClass({
+    render: function () {
+        return (
+            <div className='main-container'>
+                Hello
+            </div>
+        )
+    }
+});
+
+module.exports = Main;
